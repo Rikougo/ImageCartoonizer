1eab0ecc7ab6dc02903d1bc3acc68df27f97882a
Elye <samhelye@gmail.com>
2022-11-30 10:50:43 +0100

    git lfs & python update

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Projet de cartoonisation d'image par Sylvan LECLERC et Samuel HELYE dans le cadr
 Un rapport est posté chaque semaine dans le dossier `report`.
 
 # Rapports postés
+
 - 29/10/2022 - Premier rapport : Etat de l'art, pistes et intuitions pour le projet.
 - 05/11/2022 - Deuxième rapport : Etat d'avancement durant la semaine de pause/entreprise
-- 12/11/2022 - Troisième rapport : Recherche approfondies et pratique pour la méthode utilisant reconnaissance d'objet & génération ainsi qu'avancement sur la méthode de LPE
+- 12/11/2022 - Troisième rapport : Recherche approfondies et pratique pour la méthode utilisant reconnaissance d'objet & génération ainsi qu'avancement sur la méthode de LPE.
+- 19/11/2022 - Quatrième rapport : Corrections de bugs sur la LPE et début du gradient. Set-up de l'environnement pour la génération par OpenAI.
+- 29/11/2022 - Cinquième rapport : Avancée sur le gradient par LPE et de la segmentation et génération d'image avec OpenAI.
--- generator/main.py
+++ generator/main.py
@@ -1,13 +1,62 @@
+import sys
+
 import openai
+from imageai.Detection import ObjectDetection
+from PIL import Image, ImageDraw
 
-key_file = open("api.key", "r")
-api_key = key_file.read().strip()
+def AnalysisInput(p_detected: ObjectDetection, p_filepath: str, p_miniProbability: int = 50) -> list :
+    l_detections = p_detected.detectObjectsFromImage(
+        input_image=p_filepath,
+        output_image_path="{0}_detection.jpg".format(p_filepath.split(".")[0]),
+        minimum_percentage_probability=p_miniProbability
+    )
+    return l_detections
 
-openai.api_key = api_key
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Please specify a file path to convert")
+        sys.exit(-1)
 
-response = openai.Image.create(
-    prompt="Sylvain mange un caillou",
-    n=1,
-    size="1024x1024")
+    filename = sys.argv[1]
+    filename_noext = filename.split(".")[0]
 
-print(response['data'][0]['url'])
+    lpe_filename = sys.argv[2]
+
+    print("Loading {} ({})".format(filename, filename_noext))
+
+    key_file = open("api.key", "r")
+    api_key = key_file.read().strip()
+
+    openai.api_key = api_key
+
+    # init object detector
+    l_objectDetector = ObjectDetection()
+    l_objectDetector.setModelTypeAsYOLOv3()
+    l_objectDetector.setModelPath("yolo.h5")
+    l_objectDetector.loadModel()
+
+    l_detections = AnalysisInput(l_objectDetector, filename)
+
+    # for eachObject in l_detections:
+    #     print("{} : {}; {}".format(eachObject["name"], eachObject["percentage_probability"], eachObject["box_points"]))
+
+    l_image = Image.open(lpe_filename)
+    l_image.putalpha(255)
+
+    draw = ImageDraw.Draw(l_image)
+
+    for eachObject in l_detections:
+        print("{} : {}; {}".format(eachObject["name"], eachObject["percentage_probability"], eachObject["box_points"]))      
+
+        draw.rectangle(eachObject["box_points"], fill=(0, 0, 0, 0))
+
+        l_image.save(filename_noext + "_mask.png")
+
+        response = openai.Image.create_edit(image=open(lpe_filename, "rb"), mask=open(filename_noext + "_mask.png", "rb"), prompt="Zenyatta of overwatch on the cliff", n=1, size="512x512")
+        print(response['data'][0]['url'])
+        break
+
+
+
+    
+        
